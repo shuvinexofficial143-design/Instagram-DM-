7d4259be40a5f37b6d55329d1de95a96a901062a
shuvinexofficial143-design <shuvinex.official.143@gmail.com>
2026-09-18 13:18:58 +0530

    Add Vercel Instagram OAuth start endpoint

---
+++ api/auth/instagram.ts
@@ -0,0 +1,40 @@
+import {
+  createOAuthState,
+  getInstagramRedirectUri,
+  getOrCreateGuestWorkspaceId,
+  metaAppId,
+  metaAppSecret,
+} from '../../src/server/instagramVercel';
+
+export default function handler(req: any, res: any) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', 'GET');
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  if (!metaAppId || !metaAppSecret) {
+    return res.status(503).send(
+      'Instagram OAuth is not configured. Set INSTAGRAM_APP_ID and INSTAGRAM_APP_SECRET in Vercel.'
+    );
+  }
+
+  const workspaceId = getOrCreateGuestWorkspaceId(req, res);
+  const redirectUri = getInstagramRedirectUri(req);
+  const state = createOAuthState(workspaceId);
+  const scopes = [
+    'instagram_business_basic',
+    'instagram_business_manage_messages',
+    'instagram_business_manage_comments',
+    'instagram_business_content_publish',
+  ].join(',');
+
+  const params = new URLSearchParams({
+    client_id: metaAppId,
+    redirect_uri: redirectUri,
+    scope: scopes,
+    response_type: 'code',
+    state,
+  });
+
+  return res.redirect(302, `https://www.instagram.com/oauth/authorize?${params.toString()}`);
+}
