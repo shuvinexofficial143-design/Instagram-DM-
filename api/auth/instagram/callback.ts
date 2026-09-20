5068857ee2ad5f211bdff51753a04bd88d396baa
shuvinexofficial143-design <shuvinex.official.143@gmail.com>
2026-09-20 12:15:20 +0530

    Bind Instagram OAuth to authenticated Supabase user

--- api/auth/instagram/callback.ts
+++ api/auth/instagram/callback.ts
@@ -61,7 +61,8 @@ function getRedirectUri(req: any): string {
 }
 
 function isWorkspaceId(value: string): boolean {
-  return /^guest_[a-f0-9-]{16,}$/i.test(value);
+  return /^guest_[a-f0-9-]{16,}$/i.test(value) ||
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
 }
 
 function verifyOAuthState(rawState: unknown): string {
